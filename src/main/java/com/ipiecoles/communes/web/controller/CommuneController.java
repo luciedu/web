acc4c756d35a04681ed82e3f81e75821bc63d4e0
dumas <dumasalves.lucie@gmail.com>
2021-06-29 16:59:00 +0200

    modif commune / suppression commune / ajout commune

--- src/main/java/com/ipiecoles/communes/web/controller/CommuneController.java
+++ src/main/java/com/ipiecoles/communes/web/controller/CommuneController.java
@@ -2,10 +2,13 @@ package com.ipiecoles.communes.web.controller;
 import com.ipiecoles.communes.web.model.Commune;
 import com.ipiecoles.communes.web.repository.CommuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.Optional;
 
 
@@ -15,6 +18,7 @@ public class CommuneController {
 
     @Autowired
     private CommuneRepository communeRepository;
+
     @GetMapping("/communes/{codeInsee}")
     public String getCommune(
             @PathVariable String codeInsee,
@@ -22,6 +26,37 @@ public class CommuneController {
     {
         Optional<Commune> commune = communeRepository.findById(codeInsee);
         model.put("commune", commune.get());
+        return "detail";
+    }
+
+    @PostMapping(value ="/communes", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String saveNewCommune(
+            Commune commune,
+            final ModelMap model)
+    {
+        // Ajouter un certain nombre de contrôles...
+        commune = communeRepository.save(commune);
+        model.put("commune", commune);
+        return "detail";
+    }
+
+    @PostMapping(value ="/communes/{codeInsee}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String saveExistingCommune(
+            Commune commune,
+            @PathVariable String codeInsee,
+            final ModelMap model)
+    {
+        // Ajouter un certain nombre de contrôles...
+        commune = communeRepository.save(commune);
+        model.put("commune", commune);
+        return "detail";
+    }
+
+    @GetMapping("/communes/new")
+    public String newCommune(
+            final ModelMap model)
+    {
+        model.put("commune",new Commune());
         return "detail";
     }
 
